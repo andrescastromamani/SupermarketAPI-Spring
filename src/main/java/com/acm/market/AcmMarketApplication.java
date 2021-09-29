d8875e78fc9fbf20cfcaaa98523b1ca4c1a0a57b
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-09-29 09:41:19 -0400

    Initial config of the project API acm-market

---
+++ src/main/java/com/acm/market/AcmMarketApplication.java
@@ -0,0 +1,13 @@
+package com.acm.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acm/market/AcmMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acm.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
