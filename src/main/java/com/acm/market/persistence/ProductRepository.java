66114db3c652408202de7af9bacf1c816848ed15
Andrés Castro Mamani <castroandresrm@gmail.com>
2021-10-01 10:56:32 -0400

    We create the domain objects using data mapper

--- src/main/java/com/acm/market/persistence/ProductRepository.java
+++ src/main/java/com/acm/market/persistence/ProductRepository.java
@@ -2,10 +2,12 @@ package com.acm.market.persistence;
 
 import com.acm.market.persistence.crud.ProductCrudRepository;
 import com.acm.market.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
@@ -19,5 +21,17 @@ public class ProductRepository {
 
     public Optional<List<Product>> getStock(int mount) {
         return productCrudRepository.findByMountStockLessThanAndStatus(mount, true);
+    }
+
+    public Optional<Product> getProduct(int idProduct) {
+        return productCrudRepository.findById(idProduct);
+    }
+
+    public Product saveProduct(Product product) {
+        return productCrudRepository.save(product);
+    }
+
+    public void deleteProduct(int idProduct) {
+        productCrudRepository.deleteById(idProduct);
     }
 }
